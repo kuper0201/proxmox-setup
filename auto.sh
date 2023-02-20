bb654c74107bb0c4190b45ed39318c0c1202a397
정준수 <jeongjunsu@jeongjunsuui-MacBookAir.local>
2023-02-20 15:07:17 +0900

    automation script

---
+++ auto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/kuper0201/proxmox-setup/master/proxmox_first_setup.sh
+wget https://raw.githubusercontent.com/kuper0201/proxmox-setup/master/proxmox_gpu_passthrough.sh
+
+chmod +x proxmox_first_setup.sh
+chmod +x proxmox_gpu_passthrough.sh
+
+./proxmox_first_setup.sh
+./proxmox_gpu_passthrough.sh
---
+++ proxmox_first_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Static to DHCP
+sed -i 's/manual/dhcp/g' /etc/network/interfaces
+
+# Disable proxmox sub-repo
+echo "#`cat /etc/apt/sources.list.d/pve-enterprise.list`" > /etc/apt/sources.list.d/pve-enterprise.list
+
+# Enable proxmox community-repo
+echo -e "\ndeb http://download.proxmox.com/debian/pve bullseye pve-no-subscription​" >> /etc/apt/sources.list​

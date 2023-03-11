cfda1ab14f3f2cdaa3ec9f62640e4eb740cd0e2d
정준수 <jeongjunsu@jeongjunsuui-MacBookAir.local>
2023-03-11 17:48:30 +0900

    var

--- auto.sh
+++ auto.sh
@@ -22,7 +22,6 @@ echo "CF_Token=$CF_Token" >> cf_api.txt
 wget https://raw.githubusercontent.com/kuper0201/proxmox-setup/master/proxmox_cert.sh
 chmod +x proxmox_cert.sh
 ./proxmox_cert.sh
-rm -rf ./proxmox_cert.sh
 
 # DHCP, GPU Passthrough
 wget https://raw.githubusercontent.com/kuper0201/proxmox-setup/master/proxmox_dhcp.sh
@@ -34,21 +33,24 @@ chmod +x proxmox_gpu_passthrough.sh
 ./proxmox_dhcp.sh
 ./proxmox_gpu_passthrough.sh
 
-rm -rf ./proxmox_dhcp.sh
-rm -rf ./proxmox_gpu_passthrough.sh
-
 # CloudFlare DDNS
 wget https://raw.githubusercontent.com/kuper0201/proxmox-setup/master/update-cloudflare-dns.sh
 wget https://raw.githubusercontent.com/kuper0201/proxmox-setup/master/update-cloudflare-dns.conf
 
-sed -i 's/zoneid_here/$CF_Zone_ID/g' update-cloudflare-dns.conf
-sed -i 's/apitoken_here/$CF_Token/g' update-cloudflare-dns.conf
-sed -i 's/telegram_chat_id_here/$TG_Chat_ID/g' update-cloudflare-dns.conf
-sed -i 's/telegram_bot_token_here/$TG_Bot_Token/g' update-cloudflare-dns.conf
+sed -i "s/zoneid_here/$CF_Zone_ID/g" update-cloudflare-dns.conf
+sed -i "s/apitoken_here/$CF_Token/g" update-cloudflare-dns.conf
+sed -i "s/telegram_chat_id_here/$TG_Chat_ID/g" update-cloudflare-dns.conf
+sed -i "s/telegram_bot_token_here/$TG_Bot_Token/g" update-cloudflare-dns.conf
 
 chmod +x update-cloudflare-dns.sh
 mv update-cloudflare-dns.sh /usr/local/bin/update-cloudflare-dns
 mv update-cloudflare-dns.conf /usr/local/bin/update-cloudflare-dns.conf
 (crontab -l 2>/dev/null; echo "0 * * * * /usr/local/bin/update-cloudflare-dns") | crontab -
+
+# Cleanup
+rm -rf ./proxmox_cert.sh
+rm -rf ./proxmox_dhcp.sh
+rm -rf ./proxmox_gpu_passthrough.sh
+rm -rf ./cf_api.txt
 
 reboot

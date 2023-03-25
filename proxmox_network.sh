cad09c0f01e1e901a771cd7e4ef25cfd46a52657
kuper0201 <kuper171@gmail.com>
2023-03-25 19:22:20 +0900

    Update proxmox_network.sh

--- proxmox_network.sh
+++ proxmox_network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Static IP(192.168.0.2)
-sed -i "/address/c\address 192.168.0.2/24" /etc/network/interfaces
-sed -i "/gateway/c\gateway 192.168.0.1" /etc/network/interfaces
+sed -i "/address/c\        address 192.168.0.2/24" /etc/network/interfaces
+sed -i "/gateway/c\        gateway 192.168.0.1" /etc/network/interfaces

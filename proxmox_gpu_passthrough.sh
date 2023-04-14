c1f6aa617270b18ccd8a3ae4de3f98b9ab6460ab
kuper0201 <kuper171@gmail.com>
2023-04-15 02:59:08 +0900

    Update proxmox_gpu_passthrough.sh

--- proxmox_gpu_passthrough.sh
+++ proxmox_gpu_passthrough.sh
@@ -28,5 +28,8 @@ echo "blacklist snd_hda_intel" >> /etc/modprobe.d/blacklist.conf
 #echo "options vfio-pci ids=1002:6810,1002:aab0 disable_vga=1" > /etc/modprobe.d/vfio.conf
 
 # for NVIDIA GTX960
-echo "options vfio-pci ids=10de:1401,10de:0fba disable_vga=1" > /etc/modprobe.d/vfio.conf
+#echo "options vfio-pci ids=10de:1401,10de:0fba disable_vga=1" > /etc/modprobe.d/vfio.conf
+
+# for NVIDIA GTX1060
+echo "options vfio-pci ids=10de:10f1,10de:1c02 disable_vga=1" > /etc/modprobe.d/vfio.conf
 update-initramfs -u

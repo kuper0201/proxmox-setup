442b2f140e33cb8f72739d9765691263971e904e
kuper0201 <kuper171@gmail.com>
2023-07-06 17:39:01 +0900

    Update proxmox_repo.sh

--- proxmox_repo.sh
+++ proxmox_repo.sh
@@ -4,4 +4,4 @@
 echo "#`cat /etc/apt/sources.list.d/pve-enterprise.list`" > /etc/apt/sources.list.d/pve-enterprise.list
 
 # Enable proxmox community-repo
-echo -e "\ndeb http://download.proxmox.com/debian/pve bullseye pve-no-subscription​" >> /etc/apt/sources.list​
+echo -e "\ndeb http://download.proxmox.com/debian/pve bookworm pve-no-subscription​" >> /etc/apt/sources.list​

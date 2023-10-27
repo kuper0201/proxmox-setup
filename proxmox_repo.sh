1e741ea37cf5337a48caf454d89ea2b75e787b11
kuper0201 <kuper171@gmail.com>
2023-10-27 20:55:33 +0900

    Update proxmox_repo.sh

--- proxmox_repo.sh
+++ proxmox_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enable proxmox community-repo
-echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription​" > /etc/apt/sources.list.d/pve-enterprise.list
+echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-enterprise.list
 
 # Ceph-repo
 echo "deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription" > /etc/apt/sources.list.d/ceph.list

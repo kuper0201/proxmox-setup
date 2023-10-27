ed99525f53de621eb09f2415781d929f44b21db7
kuper0201 <kuper171@gmail.com>
2023-10-27 15:13:11 +0900

    Update proxmox_cert.sh

--- proxmox_cert.sh
+++ proxmox_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pvenode acme account register cloudflare me@jeong-jun.su
+pvenode acme account register default cloudflare me@jeong-jun.su
 pvenode acme plugin add dns cloudflare --api cf --data ./cf_api.txt --validation-delay 30
 pvenode config set --acmedomain0 jvirtual.jeong-jun.su,plugin=cloudflare
 pvenode acme cert order

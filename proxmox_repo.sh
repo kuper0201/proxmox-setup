99ce18ce8198dc9aa1e68c46dc33baf3770b3a75
kuper0201 <kuper171@gmail.com>
2023-12-17 02:17:54 +0900

    Update proxmox_repo.sh

--- proxmox_repo.sh
+++ proxmox_repo.sh
@@ -5,3 +5,6 @@ echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >
 
 # Ceph-repo
 echo "deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription" > /etc/apt/sources.list.d/ceph.list
+
+# Disable Valid Pop-Up
+sed -i "s/\tExt.Msg.show/void/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js

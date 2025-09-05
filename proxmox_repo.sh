1645d88c4d9d765baad9540f290c2763eab74bb4
kuper0201 <kuper171@gmail.com>
2025-09-06 02:07:42 +0900

    Update proxmox_repo.sh

--- proxmox_repo.sh
+++ proxmox_repo.sh
@@ -6,5 +6,8 @@ echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >
 # Ceph-repo
 echo "deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription" > /etc/apt/sources.list.d/ceph.list
 
-# Disable Valid Pop-Up
-sed -i "s/\tExt.Msg.show/void/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+# Disable Valid Pop-Up(8.0)
+# sed -i "s/\tExt.Msg.show/void/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+
+# Disable Valid Pop-Up(9.0)
+sed -i '/data\.status/{s/\!//;s/active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
